a5b0cc1e31aa6a9d013d8ce21c9c284b399e2f85
User <User@User-PC>
2018-06-29 09:17:51 +0530

    Inflow/Out

--- Java_Selenium_Prerequisite/src/junit_Test/Inflow_Outflow_MyMoneyMap.java
+++ Java_Selenium_Prerequisite/src/junit_Test/Inflow_Outflow_MyMoneyMap.java
@@ -94,9 +94,30 @@ public class Inflow_Outflow_MyMoneyMap {
 		System.out.println("inflow");
 	}
 
-	@Ignore
+	//@Ignore
 	@Test
-	public void test2() {
-		System.out.println("test2");
+	public void outflow() {
+		List<String> attribute = new ArrayList();
+		attribute.add("Transportation");
+		attribute.add("Auto");
+		
+		List<String> value = new ArrayList();
+		value.add("$160.00");
+		value.add("$102.38");
+		
+		driver.findElement(By.cssSelector("li#money_map_tab>a")).click();
+		
+		WebDriverWait wait=new WebDriverWait(driver, 50);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='gridview-1021']/table/tbody/tr[4]/td[1]/div/b")));
+		
+		List<WebElement> tr=driver.findElements(By.xpath("//tr[contains(@id,'bd-Spendings')]/td/table/tbody/tr"));
+		for(int i=1;i<attribute.size();i++)
+		{
+			String att=driver.findElement(By.xpath("//tr[contains(@id,'bd-Spendings')]/td/table/tbody/tr[contains(@class,'x-grid-row')]["+i+"]/td[1]")).getText();
+			String val=driver.findElement(By.xpath("//tr[contains(@id,'bd-Spendings')]/td/table/tbody/tr[contains(@class,'x-grid-row')]["+i+"]/td[2]")).getText();
+			Assert.assertEquals(attribute.get(i-1),att);
+			Assert.assertEquals(value.get(i-1),val);
+		}
+		System.out.println("outflow");
 	}
 }
