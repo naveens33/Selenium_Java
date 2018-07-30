63c32b25da1f0933747bcb0c425271ca404beae8
User <User@User-PC>
2018-07-30 09:35:27 +0530

    Login_Scenario_2

---
+++ Java_Selenium_Prerequisite/src/selenium_webdriver/Login_Scenario_2.java
@@ -0,0 +1,51 @@
+package selenium_webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login_Scenario_2 {
+
+	static WebDriver driver;
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Training\\Batch2\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.navigate().to("http://zero.webappsecurity.com/");
+		
+		WebElement signin=driver.findElement(By.id("signin_button"));
+		signin.click();
+		
+		driver.findElement(By.id("user_login")).sendKeys("username");
+		
+		driver.findElement(By.id("user_password")).sendKeys("pass");
+		
+		driver.findElement(By.name("submit")).click();
+		
+		if(driver.getTitle().equals("Zero - Log in"))
+		{
+			System.out.println("Pass: Title is correct");
+		}
+		else
+		{
+			System.err.println("Fail: Title is incorrect");
+		}
+		
+		if(driver.findElement(By.xpath("//*[@id='login_form']/div[1]")).getText().equals("Login and/or password are wrong."))
+		{
+			System.out.println("Pass: Error message displayed");
+		}
+		else
+		{
+			System.err.println("Fail: Error message not displayed");
+		}
+		
+			
+		driver.close();
+	}
+
+}
